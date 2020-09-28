6e2f9dd510349595bacae5501b27ce24d2abe792
ImTizzie <ImTizzie@users.noreply.github.com>
2020-09-28 14:16:23 -0500

    Updated Guard Movement
    - Moves to random spots at time intervals
    - No collision (yet)

--- Assets/Scripts/Guard/PatrolMovement.cs
+++ Assets/Scripts/Guard/PatrolMovement.cs
@@ -1,22 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PatrolMovement : MonoBehaviour
 {
     public float speed = 4f;
+    private float waitTime;
+    public float startWaitTime;
 
     public Transform[] moveSpots;
     private int randomSpot;
 
     void Start()
     {
-        randomSpot = Random.Range(0, moveSpots.Length);
+        waitTime = startWaitTime;
+        randomSpot = UnityEngine.Random.Range(0, moveSpots.Length); 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 0.02f)
+        {
+            if(waitTime < 0)
+            {
+                randomSpot = UnityEngine.Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime = waitTime - Time.deltaTime;
+            }
+        }
     }
 }
