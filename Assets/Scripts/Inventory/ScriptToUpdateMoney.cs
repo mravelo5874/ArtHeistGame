641858cadf8a215e48e84689854ec5dd796c2289
ImTizzie <ImTizzie@users.noreply.github.com>
2020-11-14 21:05:49 -0600

    Updated Shop Again
    - Couple retouches

--- Assets/Scripts/Inventory/ScriptToUpdateMoney.cs
+++ Assets/Scripts/Inventory/ScriptToUpdateMoney.cs
@@ -12,6 +12,6 @@ public class ScriptToUpdateMoney : MonoBehaviour
 
     void Update()
     {
-        moneyText.text = "Money: $" + InventoryScript.money;
+        moneyText.text = "$" + InventoryScript.money;
     }
 }
